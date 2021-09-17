d99b22d0fe6cb14801fcc808a8fe18228bb49948
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2021-09-17 15:58:48 +0530

    starting

---
+++ script/main.js
@@ -0,0 +1,6 @@
+let head = document.getElementById("main-head")
+let paragraph = document.getElementById("para")
+function entry() {
+    head.innerText = "JavaScript" ;
+    paragraph.innerText = "Welcome in the world of javascript."
+}
